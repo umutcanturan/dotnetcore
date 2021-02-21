270ea68463920184be1f9c856fed646e5e7784cf
Umutcan Turan <umutcanturan55@gmail.com>
2021-02-21 14:36:09 +0300

    example unit test functions added

---
+++ Tests/DotnetCore.Api.Test/CustomerTest.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using DotnetCore.Common.DTOs;
+using DotnetCore.Repository.Interfaces;
+using DotnetCore.Service.Implementations;
+using DotnetCore.Service.Interfaces;
+using FakeItEasy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DotnetCore.Api.Test
+{
+	public class CustomerTest
+	{
+		[Fact]
+		public async Task NewCustomerApi()
+		{
+			var customerService = A.Fake<ICustomerService>();
+			var customer = A.Fake<CustomerDTO>();
+			await customerService.NewAsync(customer);
+			A.CallTo(() => customerService.NewAsync(A<CustomerDTO>._)).MustHaveHappenedOnceExactly();
+		}
+	}
+}
---
+++ Tests/DotnetCore.Api.Test/OrderTest.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using DotnetCore.Common.DTOs;
+using DotnetCore.Repository.Interfaces;
+using DotnetCore.Service.Implementations;
+using DotnetCore.Service.Interfaces;
+using FakeItEasy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DotnetCore.Api.Test
+{
+	public class OrderTest
+	{
+		[Fact]
+		public void NewOrderService()
+		{
+			var orderRepository = A.Fake<IOrderRepository>();
+			var productRepository = A.Fake<IProductRepository>();
+			var mapper = A.Fake<IMapper>();
+			var orderService = new OrderService(productRepository, orderRepository, mapper);
+			var order = new OrderDTO()
+			{
+				Amount = 100,
+				CustomerId = 1,
+				ProductId = 1,
+				Status = Common.Enums.OrderStatus.Waiting
+			};
+			var response = orderService.New(order);
+			Assert.True(response.ResultCode == Common.Enums.ResponseResult.NotStockAmount);
+		}
+
+		[Fact]
+		public void NewOrderApi()
+		{
+			var orderService = A.Fake<IOrderService>();
+			var order = A.Fake<OrderDTO>();
+			orderService.New(order);
+			A.CallTo(() => orderService.New(A<OrderDTO>._)).MustHaveHappenedOnceExactly();
+		}
+	}
+}
